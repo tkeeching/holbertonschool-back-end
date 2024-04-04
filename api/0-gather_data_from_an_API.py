b23aa123834d6056a627115f6f55f803663f9dab
tkeeching <tkeeching@gmail.com>
2024-04-04 16:22:48 +1100

    Convert to module

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,28 +4,35 @@ import urllib.request
 import sys
 import json
 
-# Fetch employee details by id and employees todos
-employee_id = sys.argv[1]
-employee_details_response = urllib.request.urlopen(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-todos_response = urllib.request.urlopen(
-    'https://jsonplaceholder.typicode.com/todos')
+def get_todos_by_id(employee_id):
+    """For a given employee ID, returns information about his/her TODO list progess"""
 
-# Convert response to JSON
-employee_details_data = json.loads(employee_details_response.read())
-todos_data = json.loads(todos_response.read())
+    # Fetch employee details by id and employees todos
+    employee_details_response = urllib.request.urlopen(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    todos_response = urllib.request.urlopen(
+        'https://jsonplaceholder.typicode.com/todos')
 
-# Filter todos by employee's userId
-employee_todos_total = [todo for todo in todos_data
-                        if todo.get('userId') == employee_details_data['id']]
-employee_todos_completed = [todo for todo in employee_todos_total
-                            if todo.get('completed') is True]
+    # Convert response to JSON
+    employee_details_data = json.loads(employee_details_response.read())
+    todos_data = json.loads(todos_response.read())
 
-# Display result
-print('Employee {} is done with tasks({}/{}):'
-      .format(employee_details_data['name'],
-              len(employee_todos_completed),
-              len(employee_todos_total)))
+    # Filter todos by employee's userId
+    employee_todos_total = [todo for todo in todos_data
+                            if todo.get('userId') == employee_details_data['id']]
+    employee_todos_completed = [todo for todo in employee_todos_total
+                                if todo.get('completed') is True]
 
-for todo in employee_todos_completed:
-    print('\t{}'.format(todo['title']))
+    # Display result
+    print('Employee {} is done with tasks({}/{}):'
+          .format(employee_details_data['name'],
+                  len(employee_todos_completed),
+                  len(employee_todos_total)))
+
+    for todo in employee_todos_completed:
+        print('\t{}'.format(todo['title']))
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+
+    get_todos_by_id(employee_id)
