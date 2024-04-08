92b9a82766634b3bb804ccc25d75ffc9763b9680
tkeeching <tkeeching@gmail.com>
2024-04-08 14:46:11 +1000

    Test

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -37,12 +37,12 @@ def get_todos_by_id(employee_id):
         writer = csv.writer(csvfile)
         for todo in employee_todos_total:
             todo_completed_status = (
-                "'True'" if todo['completed'] else "'False'")
+                'True' if todo['completed'] else 'False')
             writer.writerow(
-                ["'" + employee_id + "'",
-                 "'" + employee_details_data['username'] + "'",
+                [employee_id,
+                 employee_details_data['username'],
                  todo_completed_status,
-                 "'" + todo['title'] + "'"])
+                 todo['title']])
 
 
 if __name__ == "__main__":
