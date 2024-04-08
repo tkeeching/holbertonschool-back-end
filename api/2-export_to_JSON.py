594a71f71f81c3db203d1c23929f28ee4e562b13
tkeeching <tkeeching@gmail.com>
2024-04-09 09:40:47 +1000

    Oops

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,57 +1,54 @@
 #!/usr/bin/python3
 """
-For a given employee ID, returns information
-about his/her TODO list progress.
+Records all tasks for an employee based in employee_id.
 Export the result in JSON format, named <user_id>.json
 """
 
-import csv
 import json
 import sys
 import urllib.request
 
 
-def get_all_employees_todos():
+def get_employee_todos(employee_id):
     """
-    Retrieve all 10 employees' todos, exports information
-    about his/her TODO list progess in JSON format
+    Records all tasks from all employees.
     """
 
-    data = {}
+    # Fetch employee details by id and employees todos
+    employee_details_response = urllib.request.urlopen(
+        'https://jsonplaceholder.typicode.com/users/{}'
+        .format(employee_id))
+    todos_response = urllib.request.urlopen(
+        'https://jsonplaceholder.typicode.com/todos')
 
-    for employee_id in range(1,10):
-        # Fetch employee details by id and employees todos
-        employee_details_response = urllib.request.urlopen(
-            'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-        todos_response = urllib.request.urlopen(
-            'https://jsonplaceholder.typicode.com/todos')
-        
-        # Convert response to JSON
-        employee_details_data = json.loads(employee_details_response.read())
-        todos_data = json.loads(todos_response.read())
+    # Convert response to JSON
+    employee_details_data = json.loads(employee_details_response.read())
+    todos_data = json.loads(todos_response.read())
 
-        # Filter todos by employee's userId
-        employee_todos_total = [todo for todo in todos_data
-                                if todo.get('userId') ==
-                                employee_details_data['id']]
+    # Filter todos by employee's userId
+    employee_todos_total = [todo for todo in todos_data
+                            if todo.get('userId') ==
+                            employee_details_data['id']]
 
-        data.update({
-            employee_id: []
-        })
+    data = {
+        employee_id: []
+    }
 
-        for todo in employee_todos_total:
-            # Export employee's todos to JSON
-            data[employee_id].append(
-                {
-                    "username": employee_details_data['username'],
-                    "task": todo['title'],
-                    "completed": todo['completed']
-                }
-            )
+    for todo in employee_todos_total:
+        # Export employee's todos to JSON
+        data[employee_id].append(
+            {
+                "username": employee_details_data['username'],
+                "task": todo['title'],
+                "completed": todo['completed']
+            }
+        )
 
-    with open('todo_all_employees.json', 'w') as jsonfile:
+    with open('{}.json'.format(employee_id), 'w') as jsonfile:
         json.dump(data, jsonfile)
 
 
 if __name__ == "__main__":
-    get_all_employees_todos()
+    employee_id = sys.argv[1]
+
+    get_employee_todos(employee_id)
